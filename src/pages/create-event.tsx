dfeb355e25a9eb2a9bc1d70d6318c963f46f285d
LandonYSplit <landonvagohughes3@gmail.com>
2024-04-14 13:35:11 -0400

    fix bottom bar

--- src/pages/create-event.tsx
+++ src/pages/create-event.tsx
@@ -113,173 +113,164 @@ export default function Events() {
 
   return (
     <>
-      <div className="relative h-screen w-screen flex-1 space-y-8">
+      <div>
         <Navbar />
-        <div className="grid justify-items-center px-3">
-          <div className="flex w-full max-w-4xl items-center">
-            <h3 className="text-2xl font-bold">Create your event</h3>
-          </div>
-          <div className="mt-4 w-full max-w-4xl">
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
-              >
-                <FormField
-                  control={form.control}
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      <FormLabel htmlFor="eventTitle">Event Title</FormLabel>
-                      <Input
-                        id="eventTitle"
-                        type="text"
-                        placeholder="Name your event"
-                        className="pl-2"
-                        {...field}
-                      />
-                    </FormItem>
-                  )}
-                />
+        <div className="relative w-screen h-screen flex-1 space-y-8">
+          <div className="grid justify-items-center mt-4 mx-3">
+            <div className="w-full sm:max-w-4xl items-center">
+              <h3 className="text-2xl font-bold">Create your event</h3>
+            </div>
+            <div className="mt-4 max-w-4xl w-full">
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel htmlFor="eventTitle">Event Title</FormLabel>
+                        <Input
+                          id="eventTitle"
+                          type="text"
+                          placeholder="Name your event"
+                          className="pl-2"
+                          {...field}
+                        />
+                      </FormItem>
+                    )}
+                  />
 
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      <FormLabel htmlFor="eventDescription">
-                        Event Description
-                      </FormLabel>
-                      <Input
-                        id="eventDescription"
-                        type="text"
-                        className="pl-2"
-                        placeholder="Describe your event"
-                        {...field}
-                      />
-                    </FormItem>
-                  )}
-                />
-                <div className="flex flex-row gap-2">
-                  <div className="flex-1">
-                    <FormField
-                      control={form.control}
-                      name="eventDate"
-                      render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                          <FormLabel htmlFor="eventDate">Event Date</FormLabel>
-                          <Popover>
-                            <PopoverTrigger asChild>
-                              <FormControl id="eventDate">
-                                <Button
-                                  variant={"outline"}
-                                  className={cn(
-                                    "pl-2 text-left font-normal",
-                                    !field.value && "text-muted-foreground",
-                                  )}
-                                >
-                                  {field.value ? (
-                                    format(field.value, "PPP")
-                                  ) : (
-                                    <span>Pick a date</span>
-                                  )}
-                                  <CalendarIcon className="ml-auto h-2 w-2 opacity-50" />
-                                </Button>
-                              </FormControl>
-                            </PopoverTrigger>
-                            <PopoverContent
-                              className="w-auto p-0"
-                              align="start"
-                            >
-                              <Calendar
-                                mode="single"
-                                selected={field.value}
-                                onSelect={field.onChange}
-                                disabled={(date) => date < new Date()}
-                                initialFocus
-                              />
-                            </PopoverContent>
-                          </Popover>
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel htmlFor="eventDescription">
+                          Event Description
+                        </FormLabel>
+                        <Input
+                          id="eventDescription"
+                          type="text"
+                          className="pl-2"
+                          placeholder="Describe your event"
+                          {...field}
+                        />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="flex flex-row gap-2">
+                    <div className="flex-1">
+                      <FormField
+                        control={form.control}
+                        name="eventDate"
+                        render={({ field }) => (
+                          <FormItem className="flex flex-col">
+                            <FormLabel htmlFor="eventDate">Event Date</FormLabel>
+                            <Popover>
+                              <PopoverTrigger asChild>
+                                <FormControl id="eventDate">
+                                  <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                      "pl-2 text-left font-normal",
+                                      !field.value && "text-muted-foreground",
+                                    )}
+                                  >
+                                    {field.value ? (
+                                      format(field.value, "PPP")
+                                    ) : (
+                                      <span>Pick a date</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-2 w-2 opacity-50" />
+                                  </Button>
+                                </FormControl>
+                              </PopoverTrigger>
+                              <PopoverContent className="w-auto p-0" align="start">
+                                <Calendar
+                                  mode="single"
+                                  selected={field.value}
+                                  onSelect={field.onChange}
+                                  disabled={(date) => date < new Date()}
+                                  initialFocus
+                                />
+                              </PopoverContent>
+                            </Popover>
 
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <div className="flex-1">
-                    <FormField
-                      control={form.control}
-                      name="eventType"
-                      render={({ ...field }) => (
-                        <FormItem className="flex flex-col">
-                          <FormLabel htmlFor="eventType">Event Type</FormLabel>
-                          <Select {...field}>
-                            <SelectTrigger
-                              id="eventType"
-                              className="w-auto pl-2"
-                            >
-                              <SelectValue placeholder="Select an event type" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              {Object.values(EventType).map((type) => (
-                                <SelectItem key={type} value={type}>
-                                  {type}
-                                </SelectItem>
-                              ))}
-                            </SelectContent>
-                          </Select>
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </div>
-                <FormField
-                  control={form.control}
-                  name="eventLocation"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      <FormLabel htmlFor="eventLocation">
-                        Event Location
-                      </FormLabel>
-                      <Input
-                        id="eventLocation"
-                        type="text"
-                        placeholder="Address of event"
-                        {...field}
-                        className="pl-2"
+                            <FormMessage />
+                          </FormItem>
+                        )}
                       />
-                    </FormItem>
-                  )}
-                />
-
-                <div className="grid w-full max-w-screen-sm items-center gap-2">
-                  <Label htmlFor="upload">Event Image</Label>
-                  <Label className="text-sm font-normal text-gray-600">
-                    This will appear at the top of your event page and on the
-                    home page across our platform
-                  </Label>
-                  <Button
-                    id="upload"
-                    type="button"
-                    variant="outline"
-                    className="w-24"
-                  >
-                    <div className="mr-1">
-                      <UploadIcon />
                     </div>
-                    Upload
-                  </Button>
-                </div>
-                <EventSponsors
-                  eventId={form.getValues("eventId")}
-                  isReadyToSubmit={isReadyToSubmit}
-                />
-                <EventCollaborators
-                  eventId={form.getValues("eventId")}
-                  isReadyToSubmit={isReadyToSubmit}
-                />
-              </form>
-            </Form>
+                    <div className="flex-1">
+                      <FormField
+                        control={form.control}
+                        name="eventType"
+                        render={({ ...field }) => (
+                          <FormItem className="flex flex-col">
+                            <FormLabel htmlFor="eventType">Event Type</FormLabel>
+                            <Select {...field}>
+                              <SelectTrigger
+                                id="eventType"
+                                className="w-auto pl-2"
+                              >
+                                <SelectValue placeholder="Select an event type" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                {Object.values(EventType).map((type) => (
+                                  <SelectItem key={type} value={type}>
+                                    {type}
+                                  </SelectItem>
+                                ))}
+                              </SelectContent>
+                            </Select>
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  </div>
+                  <FormField
+                    control={form.control}
+                    name="eventLocation"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel htmlFor="eventLocation">
+                          Event Location
+                        </FormLabel>
+                        <Input
+                          id="eventLocation"
+                          type="text"
+                          placeholder="Address of event"
+                          {...field}
+                          className="pl-2"
+                        />
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="grid w-full max-w-screen-sm items-center gap-2">
+                    <Label htmlFor="upload">Event Image</Label>
+                    <Label className="text-sm font-normal text-gray-600">
+                      This will appear at the top of your event page and on the home
+                      page across our platform
+                    </Label>
+                    <Button id="upload" type="button" variant="outline" className="w-24">
+                      <div className="mr-1">
+                        <UploadIcon />
+                      </div>
+                      Upload
+                    </Button>
+                  </div>
+                  <EventSponsors
+                    eventId={form.getValues("eventId")}
+                    isReadyToSubmit={isReadyToSubmit}
+                  />
+                  <EventCollaborators
+                    eventId={form.getValues("eventId")}
+                    isReadyToSubmit={isReadyToSubmit}
+                  />
+                </form>
+              </Form>
+            </div>
           </div>
         </div>
         <div className="z-11 sticky bottom-0 flex w-full justify-end gap-2 overflow-visible border-t border-gray-200 bg-white pb-2 pr-4 pt-2">
