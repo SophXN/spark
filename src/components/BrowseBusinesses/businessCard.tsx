d68a20b7faa304ef66fabd65b7d6ffa62bee672e
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2024-04-27 10:59:22 -0400

    remove sponsor and fix business component type errors

--- src/components/BrowseBusinesses/businessCard.tsx
+++ src/components/BrowseBusinesses/businessCard.tsx
@@ -1,93 +1,120 @@
-"use client"
+"use client";
 
 import React from "react";
 import { Badge } from "../ui/badge";
-import { Facebook, Globe2, Home, Instagram, SendIcon, Twitter } from "lucide-react";
-import { TwitterLogoIcon } from "@radix-ui/react-icons";
+import {
+  Facebook,
+  Globe2,
+  Home,
+  Instagram,
+  SendIcon,
+  Twitter,
+} from "lucide-react";
 import { Button } from "../ui/button";
-import { BrowseCompanies } from "~/types/types";
+import { type BrowseCompanies } from "~/types/types";
 import { mcc } from "~/utils/mcc";
 
 interface BusinessCardProps {
-    companyData: BrowseCompanies; // Using the extended type here
+  companyData: BrowseCompanies; // Using the extended type here
 }
 
 export default function BusinessCard({ companyData }: BusinessCardProps) {
+  function findMerchantByMcc(mccCode: string) {
+    return mcc.find((merchant) => merchant.mcc === mccCode)?.value;
+  }
 
-    function findMerchantByMcc(mccCode: string) {
-        return mcc.find(merchant => merchant.mcc === mccCode)?.value;
-    }
+  const concatenatedCities = companyData.locations.reduce(
+    (accumulator, currentValue) => {
+      return accumulator + (accumulator ? ", " : "") + currentValue.city;
+    },
+    "",
+  );
 
-    const concatenatedCities = companyData.locations.reduce((accumulator, currentValue) => {
-        return accumulator + (accumulator ? ', ' : '') + currentValue.city;
-    }, '');
+  const concatenatedTypes = companyData.locations.reduce(
+    (accumulator, currentValue) => {
+      return accumulator + (accumulator ? ", " : "") + currentValue.type;
+    },
+    "",
+  );
 
-    const concatenatedTypes = companyData.locations.reduce((accumulator, currentValue) => {
-        return accumulator + (accumulator ? ', ' : '') + currentValue.type;
-    }, '');
-
-    return (
-        <div className="mt-4 w-full bg-slate-50 rounded-md p-3 flex flex-row flex-wrap">
-            <div className="flex flex-col flex-grow">
-                <div className="flex flex-row gap-2 items-center">
-                    <div className="flex-shrink-0">
-                        <img
-                            className="h-[70px] w-[70px] rounded-full"
-                            src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            alt=""
-                        />
-                    </div>
-                    <div className="min-w-0 flex flex-col gap-1">
-                        <div>
-                            <p className="text-2xl font-bold text-gray-900">
-                                {companyData.name}
-                            </p>
-                        </div>
-                        <div className="flex flex-row gap-1 mt-0.5">
-                            <a href="">
-                                <Twitter size={"18"} className="text-slate-700"></Twitter>
-                            </a>
-                            <a href="">
-                                <Instagram size={"18"} className="text-slate-700"></Instagram>
-                            </a>
-                            <a href="">
-                                <Facebook size={"18"} className="text-slate-700"></Facebook>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="mt-2">
-                    <p className="text-sm">{companyData.businessDescription}</p>
-                </div>
-                <div className="mt-2 flex flex-row gap-1">
-                    {companyData.locations.map((location) => {
-                        return <Badge key={location.merchantCode}>{findMerchantByMcc(location.merchantCode)}</Badge>
-                    })}
-                </div>
-                <div className="mt-2 flex flex-row gap-1 items-center">
-                    <Globe2 size={"20"}></Globe2>
-                    <p className="text-muted-foreground text-sm">{concatenatedCities}</p>
-                    <p className="font-extrabold">·</p>
-                    <Home size={"20"}></Home>
-                    <p className="text-muted-foreground text-sm">{concatenatedTypes}</p>
-                </div>
-                <div className="mt-2 flex flex-row flex-wrap gap-2 items-center">
-                    <div className="flex flex-row gap-1">
-                        <p className="text-sm">Collaborations:</p>
-                        <p className="text-sm font-bold">{companyData.collaboratorResponses.filter(item => item.status === "ACCEPTED").length}</p>
-                    </div>
-                    <div className="flex flex-row gap-1">
-                        <p className="text-sm">Events thrown:</p>
-                        <p className="text-sm font-bold">{companyData._count.eventRequests}</p>
-                    </div>
-                    <div className="flex flex-row gap-1">
-                        <p className="text-sm">Collab requests:</p>
-                        <p className="text-sm font-bold">{companyData._count.collaboratorResponses}</p>
-                    </div>
-                </div>
-
+  return (
+    <div className="mt-4 flex w-full flex-row flex-wrap rounded-md bg-slate-50 p-3">
+      <div className="flex flex-grow flex-col">
+        <div className="flex flex-row items-center gap-2">
+          <div className="flex-shrink-0">
+            <img
+              className="h-[70px] w-[70px] rounded-full"
+              src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
+          <div className="flex min-w-0 flex-col gap-1">
+            <div>
+              <p className="text-2xl font-bold text-gray-900">
+                {companyData.name}
+              </p>
             </div>
-            <Button className="px-2 mt-2 sm:mt-0 w-full sm:w-auto"><SendIcon size={"17"} className="mr-1" /><span className="text-sm">Invite to Collab</span></Button>
+            <div className="mt-0.5 flex flex-row gap-1">
+              <a href="">
+                <Twitter size={"18"} className="text-slate-700"></Twitter>
+              </a>
+              <a href="">
+                <Instagram size={"18"} className="text-slate-700"></Instagram>
+              </a>
+              <a href="">
+                <Facebook size={"18"} className="text-slate-700"></Facebook>
+              </a>
+            </div>
+          </div>
         </div>
-    )
+        <div className="mt-2">
+          <p className="text-sm">{companyData.businessDescription}</p>
+        </div>
+        <div className="mt-2 flex flex-row gap-1">
+          {companyData.locations.map((location) => {
+            return (
+              <Badge key={location.merchantCode}>
+                {findMerchantByMcc(location.merchantCode)}
+              </Badge>
+            );
+          })}
+        </div>
+        <div className="mt-2 flex flex-row items-center gap-1">
+          <Globe2 size={"20"}></Globe2>
+          <p className="text-sm text-muted-foreground">{concatenatedCities}</p>
+          <p className="font-extrabold">·</p>
+          <Home size={"20"}></Home>
+          <p className="text-sm text-muted-foreground">{concatenatedTypes}</p>
+        </div>
+        <div className="mt-2 flex flex-row flex-wrap items-center gap-2">
+          <div className="flex flex-row gap-1">
+            <p className="text-sm">Collaborations:</p>
+            <p className="text-sm font-bold">
+              {
+                companyData.collaboratorResponses.filter(
+                  (item) => item.status === "ACCEPTED",
+                ).length
+              }
+            </p>
+          </div>
+          <div className="flex flex-row gap-1">
+            <p className="text-sm">Events thrown:</p>
+            <p className="text-sm font-bold">
+              {companyData._count.eventRequests}
+            </p>
+          </div>
+          <div className="flex flex-row gap-1">
+            <p className="text-sm">Collab requests:</p>
+            <p className="text-sm font-bold">
+              {companyData._count.collaboratorResponses}
+            </p>
+          </div>
+        </div>
+      </div>
+      <Button className="mt-2 w-full px-2 sm:mt-0 sm:w-auto">
+        <SendIcon size={"17"} className="mr-1" />
+        <span className="text-sm">Invite to Collab</span>
+      </Button>
+    </div>
+  );
 }
--- src/components/BrowseBusinesses/filters.tsx
+++ src/components/BrowseBusinesses/filters.tsx
@@ -1,179 +1,205 @@
-"use client"
+"use client";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { mcc } from "~/utils/mcc";
-import { useState, useEffect } from 'react'
-import { Check, ChevronsUpDown } from "lucide-react"
+import { useState, useEffect } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from "~/components/ui/select"
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 
-import { cn } from "~/lib/utils"
-import { Button } from "~/components/ui/button"
+import { cn } from "~/lib/utils";
+import { Button } from "~/components/ui/button";
 import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList
-} from "~/components/ui/command"
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "~/components/ui/command";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "~/components/ui/popover"
-import { BusinessType, filterObject, BrowseMerchantsQuery } from "~/types/types";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { BusinessType, type filterObject } from "~/types/types";
 
 import React from "react";
 
 interface filterProps {
-    onChange: (value: filterObject) => void;
+  onChange: (value: filterObject) => void;
 }
 
 const Filters: React.FC<filterProps> = ({ onChange }) => {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+  const [, setInputValue] = useState("");
+  const [filterObject, setFilterObject] = React.useState<filterObject>({
+    city: "",
+    merchantCode: "",
+    type: "",
+  });
 
-    const [open, setOpen] = React.useState(false)
-    const [value, setValue] = React.useState("")
-    const [inputValue, setInputValue] = useState('');
-    const [filterObject, setFilterObject] = React.useState<filterObject>({ city: "", merchantCode: "", type: "" })
+  useEffect(() => {
+    console.log("updated");
+    console.log(filterObject, "<= before query");
 
-    useEffect(() => {
-        console.log("updated")
-        console.log(filterObject, "<= before query");
+    if (
+      filterObject.city === "" &&
+      filterObject.merchantCode === "" &&
+      filterObject.type === ""
+    ) {
+      onChange(filterObject);
+      return;
+    }
 
-        if(filterObject.city === "" && filterObject.merchantCode === "" && filterObject.type === "") {
-            onChange(filterObject);
-            return;
-        }
+    if (filterObject.city === "") {
+      delete filterObject.city;
+    }
+    if (filterObject.merchantCode === "") {
+      delete filterObject.merchantCode;
+    }
+    if (filterObject.type === "") {
+      delete filterObject.type;
+    }
+    onChange(filterObject);
+  }, [filterObject, onChange]);
 
-        if (filterObject.city === "") {
-            delete filterObject.city;
-        }
-        if (filterObject.merchantCode === "") {
-            delete filterObject.merchantCode
-        }
-        if (filterObject.type === "") {
-            delete filterObject.type
-        }
-        onChange(filterObject);
-    }, [filterObject])
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ): void => {
+    console.log(event);
+    if (event.key === "Enter") {
+      filterObject.city = event.currentTarget.value;
+      setFilterObject((prevState) => ({
+        ...prevState,
+        city: filterObject.city,
+      }));
+    }
+  };
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
-        console.log(event)
-        if (event.key === 'Enter') {
-            filterObject.city = event.currentTarget.value;
-            setFilterObject(prevState => ({
-                ...prevState,
-                city: filterObject.city
-            }));
-        }
-    };
+  const handleInputChange = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ): void => {
+    setInputValue(event.currentTarget.value); // Update the temporary input value
+  };
 
-    const handleInputChange = (event: React.KeyboardEvent<HTMLInputElement>): void => {
-        setInputValue(event.currentTarget.value); // Update the temporary input value
-    };
-
-    return (
-        <div className="flex flex-row flex-wrap gap-2">
-            <div className="w-full sm:w-64">
-                <div className="grid items-center gap-1.5">
-                    <Label>Filter by city</Label>
-                    <Input
-                        id="locationSearch"
-                        type="text"
-                        placeholder="Type a city"
-                        className="pl-2"
-                        defaultValue={filterObject.city}
-                        onKeyDown={handleKeyDown}
-                        onChange={() => handleInputChange}
-                    />
-                </div>
-            </div>
-            <div className="w-full sm:w-64">
-                <div className="grid items-center gap-1.5">
-                    <Label>Filter by category</Label>
-                    <Popover open={open} onOpenChange={setOpen}>
-                        <PopoverTrigger asChild>
-                            <Button
-                                variant="outline"
-                                role="combobox"
-                                aria-expanded={open}
-                                className="justify-between sm:w-64 overflow-hidden px-2"
-                            >
-                                {value
-                                    ? mcc.find((framework) => framework.value === value)?.label
-                                    : "Select a category..."}
-                                <ChevronsUpDown className="ml-2 h-2 w-2 shrink-0 opacity-50" />
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className="p-0 w-auto">
-                            <Command>
-                                <CommandInput placeholder="Search framework..." className='border-0 focus:ring-0' />
-                                <CommandList>
-                                    <CommandEmpty>No category found, try again.</CommandEmpty>
-                                    <CommandGroup>
-                                        {mcc.map((framework) => (
-                                            <CommandItem
-                                                key={framework.value}
-                                                value={framework.value}
-                                                onSelect={(currentValue) => {
-                                                    setValue(currentValue === value ? "" : currentValue)
-                                                    setOpen(false)
-                                                    filterObject.merchantCode = currentValue;
-                                                    setFilterObject(prevState => ({
-                                                        ...prevState,
-                                                        merchantCode: framework.mcc
-                                                    }));
-                                                }}
-                                            >
-                                                <Check
-                                                    className={cn(
-                                                        "mr-2 h-2 w-2",
-                                                        value === framework.value ? "opacity-100" : "opacity-0"
-                                                    )}
-                                                />
-                                                {framework.label}
-                                            </CommandItem>
-                                        ))}
-                                    </CommandGroup>
-                                </CommandList>
-                            </Command>
-                        </PopoverContent>
-                    </Popover>
-                </div>
-            </div>
-            <div className="w-full sm:w-64">
-                <div className="grid items-center gap-1.5">
-                    <Label>Filter by business type</Label>
-                    <Select onValueChange={(e) => {
-                        setFilterObject(prevState => ({
-                            ...prevState,
-                            type: e as BusinessType
-                        }));
-                    }}>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Select an option" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectItem value={BusinessType.MOBILE}>{BusinessType.MOBILE.toUpperCase() + BusinessType.MOBILE.slice(1).toLowerCase()}</SelectItem>
-                                <SelectItem value={BusinessType.PHYSICAL}>{BusinessType.PHYSICAL.toUpperCase() + BusinessType.PHYSICAL.slice(1).toLowerCase()}</SelectItem>
-                                <SelectItem value={BusinessType.VIRTUAL}>{BusinessType.VIRTUAL.toUpperCase() + BusinessType.VIRTUAL.slice(1).toLowerCase()}</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-row flex-wrap gap-2">
+      <div className="w-full sm:w-64">
+        <div className="grid items-center gap-1.5">
+          <Label>Filter by city</Label>
+          <Input
+            id="locationSearch"
+            type="text"
+            placeholder="Type a city"
+            className="pl-2"
+            defaultValue={filterObject.city}
+            onKeyDown={handleKeyDown}
+            onChange={() => handleInputChange}
+          />
         </div>
-    )
-}
+      </div>
+      <div className="w-full sm:w-64">
+        <div className="grid items-center gap-1.5">
+          <Label>Filter by category</Label>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="justify-between overflow-hidden px-2 sm:w-64"
+              >
+                {value
+                  ? mcc.find((framework) => framework.value === value)?.label
+                  : "Select a category..."}
+                <ChevronsUpDown className="ml-2 h-2 w-2 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Command>
+                <CommandInput
+                  placeholder="Search framework..."
+                  className="border-0 focus:ring-0"
+                />
+                <CommandList>
+                  <CommandEmpty>No category found, try again.</CommandEmpty>
+                  <CommandGroup>
+                    {mcc.map((framework) => (
+                      <CommandItem
+                        key={framework.value}
+                        value={framework.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue);
+                          setOpen(false);
+                          filterObject.merchantCode = currentValue;
+                          setFilterObject((prevState) => ({
+                            ...prevState,
+                            merchantCode: framework.mcc,
+                          }));
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-2 w-2",
+                            value === framework.value
+                              ? "opacity-100"
+                              : "opacity-0",
+                          )}
+                        />
+                        {framework.label}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+      <div className="w-full sm:w-64">
+        <div className="grid items-center gap-1.5">
+          <Label>Filter by business type</Label>
+          <Select
+            onValueChange={(e) => {
+              setFilterObject((prevState) => ({
+                ...prevState,
+                type: e as BusinessType,
+              }));
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select an option" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value={BusinessType.MOBILE}>
+                  {BusinessType.MOBILE.toUpperCase() +
+                    BusinessType.MOBILE.slice(1).toLowerCase()}
+                </SelectItem>
+                <SelectItem value={BusinessType.PHYSICAL}>
+                  {BusinessType.PHYSICAL.toUpperCase() +
+                    BusinessType.PHYSICAL.slice(1).toLowerCase()}
+                </SelectItem>
+                <SelectItem value={BusinessType.VIRTUAL}>
+                  {BusinessType.VIRTUAL.toUpperCase() +
+                    BusinessType.VIRTUAL.slice(1).toLowerCase()}
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Filters;
