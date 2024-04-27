1f47dd2997a0de4df2c379b00212b41cce45e2d9
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2024-04-27 11:05:16 -0400

    remove util and slideover

--- src/components/utils.ts
+++
@@ -1,7 +0,0 @@
-import { ServiceType } from "@prisma/client";
-
-function enumToStringArray(enumObj: any): string[] {
-    return Object.keys(enumObj).filter(key => isNaN(Number(key)));
-}
-
-export const serviceTypeKeys = enumToStringArray(ServiceType);
