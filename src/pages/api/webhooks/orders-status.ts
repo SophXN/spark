1366ff853cf0d0ed1ef6bf67eab4c7cfc6a4fb69
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2024-04-29 16:36:41 -0400

    meow

--- src/pages/api/webhooks/orders-status.ts
+++ src/pages/api/webhooks/orders-status.ts
@@ -33,7 +33,9 @@ export default async function handler(
     const body = JSON.stringify(req.body);
     const signature = req.headers["x-square-hmacsha256-signature"] as string;
     if (isFromSquare(signature, body)) {
-      res.status(200).end();
+      res
+        .status(200)
+        .json({ message: `hihihi ${req.body.data.object.payment.status}` });
       const orderId = req.body.data.id as string;
       const paymentOrderId = req.body.data.object.payment.order_id as string;
       const paymentStatus = req.body.data.object.payment.status as string;
@@ -77,4 +79,69 @@ export default async function handler(
 // if (req.body.data.type === "order_fulfillment_updated") {
 //   const orderId = req.body.data.id as string;
 //   mutation.mutate({ orderId });
+// }
+
+// {
+//   "merchant_id": "6SSW7HV8K2ST5",
+//   "type": "payment.updated",
+//   "event_id": "6a8f5f28-54a1-4eb0-a98a-3111513fd4fc",
+//   "created_at": "2020-02-06T21:27:34.308Z",
+//   "data": {
+//     "type": "payment",
+//     "id": "hYy9pRFVxpDsO1FB05SunFWUe9JZY",
+//     "object": {
+//       "payment": {
+//         "amount_money": {
+//           "amount": 100,
+//           "currency": "USD"
+//         },
+//         "approved_money": {
+//           "amount": 100,
+//           "currency": "USD"
+//         },
+//         "card_details": {
+//           "avs_status": "AVS_ACCEPTED",
+//           "card": {
+//             "bin": "540988",
+//             "card_brand": "MASTERCARD",
+//             "card_type": "CREDIT",
+//             "exp_month": 11,
+//             "exp_year": 2022,
+//             "fingerprint": "sq-1-Tvruf3vPQxlvI6n0IcKYfBukrcv6IqWr8UyBdViWXU2yzGn5VMJvrsHMKpINMhPmVg",
+//             "last_4": "9029",
+//             "prepaid_type": "NOT_PREPAID"
+//           },
+//           "card_payment_timeline": {
+//             "authorized_at": "2020-11-22T21:16:51.198Z",
+//             "captured_at": "2020-11-22T21:19:00.832Z"
+//           },
+//           "cvv_status": "CVV_ACCEPTED",
+//           "entry_method": "KEYED",
+//           "statement_description": "SQ *DEFAULT TEST ACCOUNT",
+//           "status": "CAPTURED"
+//         },
+//         "created_at": "2020-11-22T21:16:51.086Z",
+//         "delay_action": "CANCEL",
+//         "delay_duration": "PT168H",
+//         "delayed_until": "2020-11-29T21:16:51.086Z",
+//         "id": "hYy9pRFVxpDsO1FB05SunFWUe9JZY",
+//         "location_id": "S8GWD5R9QB376",
+//         "order_id": "03O3USaPaAaFnI6kkwB1JxGgBsUZY",
+//         "receipt_number": "hYy9",
+//         "receipt_url": "https://squareup.com/receipt/preview/hYy9pRFVxpDsO1FB05SunFWUe9JZY",
+//         "risk_evaluation": {
+//           "created_at": "2020-11-22T21:16:51.198Z",
+//           "risk_level": "NORMAL"
+//         },
+//         "source_type": "CARD",
+//         "status": "COMPLETED",
+//         "total_money": {
+//           "amount": 100,
+//           "currency": "USD"
+//         },
+//         "updated_at": "2020-11-22T21:19:00.831Z",
+//         "version_token": "bhC3b8qKJvNDdxqKzXaeDsAjS1oMFuAKxGgT32HbE6S6o"
+//       }
+//     }
+//   }
 // }
