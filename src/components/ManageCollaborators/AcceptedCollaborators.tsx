98556e690c24ecb02c17199bc959edc436af547c
LandonYSplit <landonvagohughes3@gmail.com>
2024-04-29 19:59:14 -0400

    updated profile images on the collab requests

--- src/components/ManageCollaborators/AcceptedCollaborators.tsx
+++ src/components/ManageCollaborators/AcceptedCollaborators.tsx
@@ -14,6 +14,7 @@ import { api } from "~/utils/api";
 import { useQueryClient } from "@tanstack/react-query";
 import { type CollaboratorResponseExtended } from "~/types/types";
 import EmptyState from "../EmptyState";
+import Image from "next/image";
 
 interface AcceptedDataProps {
   acceptedCollaborators: CollaboratorResponseExtended[];
@@ -52,13 +53,30 @@ export const AcceptedCollaborators = ({
             <Card className="mt-2" key={item.id}>
               <CardHeader>
                 <div className="relative flex items-center space-x-2 rounded-lg">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-12 w-12 rounded-full"
-                      src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                      alt=""
-                    />
-                  </div>
+                  {item.responder.profilePicture ? (
+                    <div className="flex-shrink-0">
+                      <Image
+                        className="h-12 w-12 object-cover rounded-full"
+                        src={item.responder.profilePicture}
+                        alt="Business Logo"
+                        width="100"
+                        height="100"
+                        layout="fixed"
+                      />
+                    </div>
+                  ) : (
+                    <div className="h-12 w-12 flex-shrink-0">
+                      <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                        <svg
+                          className="h-full w-full text-gray-300"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
+                      </span>
+                    </div>
+                  )}
                   <div className="min-w-0 flex-1">
                     <a>
                       <p className="text-sm font-medium text-gray-900">
