c9155a754922a15bd3b7c30f8eb81d3913fde294
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2024-04-29 21:27:33 -0400

    change role

--- src/utils/supabase/admin.ts
+++ src/utils/supabase/admin.ts
@@ -5,7 +5,7 @@ import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseAdmin: SupabaseClient = createClient(
   process.env.SUPABASE_URL ?? "",
-  process.env.SUPABASE_ANON_KEY ?? "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
 );
 
 export const updatePaymentLinkStatus = async (
